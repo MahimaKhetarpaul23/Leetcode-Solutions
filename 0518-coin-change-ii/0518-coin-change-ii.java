79fce8f4115b111b66dae83f02788a77a41543f1
Mahima <118675477+MahimaKhetarpaul23@users.noreply.github.com>
2023-08-12 01:11:05 +0530

    Time: 9 ms (35.00%), Space: 46.2 MB (21.47%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -19,16 +19,52 @@ class Solution {
     }
     
     
-    
-    public int change(int amount, int[] coins) {
+    // Tabulation:
+    public int NOW(int amount,int[] coins){
         int n=coins.length;
         int[][] dp = new int[n+1][amount+1];
+        
         for(int i=0;i<=n;i++){
             for(int j=0;j<=amount;j++){
-                dp[i][j]=-1;
+                if(i==0){
+                    dp[i][j]=0;
+                }
+                if(j==0){
+                    dp[i][j]=1;
+                }
             }
         }
         
-        return noOfWays(coins,amount,dp,0);
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=amount;j++){
+                if(coins[i-1]<=j){
+                    dp[i][j] = dp[i][j-coins[i-1]] + dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        
+        return dp[n][amount];
+    }
+    
+    
+    
+    public int change(int amount, int[] coins) {
+        
+        
+//         int n=coins.length;
+//         int[][] dp = new int[n+1][amount+1];
+//         for(int i=0;i<=n;i++){
+//             for(int j=0;j<=amount;j++){
+//                 dp[i][j]=-1;
+//             }
+//         }
+        
+//         return noOfWays(coins,amount,dp,0);
+        
+        return NOW(amount,coins);
     }
 }
